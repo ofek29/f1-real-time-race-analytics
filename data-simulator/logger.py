5e23428205ccbcef86bf295bb38aabf883de1beb
ofek29 <ofek.av95@gmail.com>
2025-03-06 16:06:25 +0200

    Improve logger setup, create only if not exist

--- data-simulator/logger.py
+++ data-simulator/logger.py
@@ -3,14 +3,19 @@ import os
 
 
 def setup_logger():
-    log_level = os.getenv("LOG_LEVEL", "DEBUG")
-    log_format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-
-    logging.basicConfig(
-        level=log_level, format=log_format, handlers=[logging.StreamHandler()]
-    )
-
     logger = logging.getLogger("dataSimLogger")
+
+    # Only configure the logger if it doesn't already have handlers
+    if not logger.handlers:
+        log_level = os.getenv("LOG_LEVEL", "DEBUG")
+        log_format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+
+        handler = logging.StreamHandler()
+        formatter = logging.Formatter(log_format)
+        handler.setFormatter(formatter)
+        logger.setLevel(log_level)
+        logger.addHandler(handler)
+
     return logger
 
 
